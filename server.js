90f4532424f20676f3f153d98bb53ac51f3567d8
sarrazol <sebastianarrazola@gmail.com>
2020-09-22 21:51:39 -0400

    added handlebars init parameters to server.js

--- server.js
+++ server.js
@@ -16,6 +16,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+// Set Handlebars.
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 // Integrate express-session and passport to allow the authentication middleware
 app.use(session({ secret: "arthur", resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
