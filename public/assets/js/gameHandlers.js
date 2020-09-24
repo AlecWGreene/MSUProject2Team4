52ec96203b71ea2f38796d82d9a96897b1e970fc
sarrazol <sebastianarrazola@gmail.com>
2020-09-23 23:38:14 -0400

    added party select function to gameHandlers.js

--- public/assets/js/gameHandlers.js
+++ public/assets/js/gameHandlers.js
@@ -3,10 +3,36 @@
 // --------------- Party Selection Modal Handlers ---------------
 function partySelectionItemToggle(event) {
 
+    let target = $(event.target);
+    
+    if (target.hasClass("selection-item-active")) {
+
+        target.removeClass("selection-item-active")
+
+    }else{
+
+        target.addClass("selection-item-active")
+
+    }
+
 }
 
 function partySelectionFinishHandler(event) {
 
+    let userArray = $(".selection-item-active").map(item => $(item).data("id"));
+
+    console.log(userArray);
+
+  $.post("/api/game/aaaa/partySelection", {
+    userIds: userArray
+  })
+    .then(() => {
+      window.location.replace("/home");
+      // If there's an error, log the error
+    })
+    .fail(err => {
+      console.log(err);
+    });
 }
 
 function partySelectionResetHandler(event) {
