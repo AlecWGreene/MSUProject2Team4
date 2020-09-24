22b37b2a4bb0d2767dc729c68e0bd18299544598
Alec W Greene <alecwgreene@gmail.com>
2020-09-24 01:38:18 -0400

    Implemented SessionManager with static methods

--- config/sessionManager.js
+++ config/sessionManager.js
@@ -1,19 +1,7 @@
 const { GameSession: GameSession } = require("./gameSession.js");
 
 class SessionManager {
-  constructor() {
-    this.sessionDictionary = {};
-  }
-
-  static getInstance() {
-    if (SessionManager.instance === null) {
-      const sessionManager = new SessionManager();
-      SessionManager.instance = sessionManager;
-    }
-    return SessionManager.instance;
-  }
-
-  createSession(lobbyCode, customSettings) {
+  static createSession(lobbyCode, customSettings) {
     // Register new session
     this.sessionDictionary[lobbyCode] = new GameSession(
       lobbyCode,
@@ -23,11 +11,11 @@ class SessionManager {
     /** @todo Load users from lobby into GameSession.users */
   }
 
-  resolveSession() {
+  static resolveSession(lobbyCode) {
     this.sessionDictionary[lobbyCode] = undefined;
   }
 }
 
-SessionManager.instance = null;
+SessionManager.sessionDictionary = {};
 
 module.exports = SessionManager;
