ca9ff3fa3b8ec4576d799a2c35a5cb664c3b5302
sarrazol <sebastianarrazola@gmail.com>
2020-09-24 22:29:42 -0400

    edited html-routes.js and server.js

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,16 +1,14 @@
-// Requiring path to so we can use relative routes to our HTML files
-const path = require("path");
-
 // Requiring our custom middleware for checking if a user is logged in
 const isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function(app) {
   app.get("/", (req, res) => {
-    // If the user already has an account send them to the members page
+    // If the user already has an account send them to user home page.
     if (req.user) {
       res.redirect("/home");
     }
-    res.sendFile(path.join(__dirname, "../public/signup.html"));
+    // render rules page if not yet logged in.
+    res.render("homepage");
   });
 
   app.get("/login", (req, res) => {
@@ -25,7 +23,7 @@ module.exports = function(app) {
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/home", isAuthenticated, (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/home.html"));
+    res.render("homepage", { layout: "userPage" });
   });
 
   // Render games page
