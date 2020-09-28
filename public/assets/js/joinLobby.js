14aed99555490ff3e6f7b8d07d36f37bf30c7a8b
Alec W Greene <alecwgreene@gmail.com>
2020-09-28 18:37:30 -0400

    fixed stuff

--- public/assets/js/joinLobby.js
+++ public/assets/js/joinLobby.js
@@ -73,7 +73,9 @@ $(document).ready(() => {
 
     function joinlobby(idHash){
         $.post(`/api/lobby/join/${idHash}`)
-        .then(res => console.log(res))
+        .then(res => {
+            window.location.pathname = "/lobby/wait";
+        })
 
         .catch(handleLoginErr);
 
