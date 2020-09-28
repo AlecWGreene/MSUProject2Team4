75d9c2a929a9ab88a0952af805a0a4084ee0f638
sarrazol <sebastianarrazola@gmail.com>
2020-09-27 23:30:24 -0400

    updated join lobby page - debugging user join lobby route

--- public/assets/js/joinLobby.js
+++ public/assets/js/joinLobby.js
@@ -16,19 +16,19 @@ $(document).ready(() => {
                     ` <li class="list-group-item">
                         <div class="row">
                             <div class="col-12 d-flex justify-content-center">
-                                <div class="list-group-item list-group-item-action lobby-items" id="lobby-item-i">
+                                <div class="list-group-item list-group-item-action lobby-items" id="${i}-${lobbiesInfo[i].idhash}-lobbyitems">
                                     <div class="row">
-                                        <div class="col-11" id="lobby-name-i">
-                                            ${lobbiesInfo[0].lobbyname}
+                                        <div class="col-11 lobby-name" id="${i}-${lobbiesInfo[i].idhash}-lobbyname">
+                                            ${lobbiesInfo[i].lobbyname}
                                         </div>
-                                        <div class="col-1" id="add-lobby-i">
+                                        <div class="col-1 add-lobby" id="${i}-${lobbiesInfo[i].idhash}-addlobby">
                                             <svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-person-plus-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                                 <path fill-rule="evenodd" d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm7.5-3a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"/>
                                             </svg>
                                         </div>
-                                        <div class="col-1 hide" id="remove-lobby-i">
-                                            <svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-person-x" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                                <path fill-rule="evenodd" d="M8 5a2 2 0 1 1-4 0 2 2 0 0 1 4 0zM6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm6 5c0 1-1 1-1 1H1s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C9.516 10.68 8.289 10 6 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10zm1.146-7.85a.5.5 0 0 1 .708 0L14 6.293l1.146-1.147a.5.5 0 0 1 .708.708L14.707 7l1.147 1.146a.5.5 0 0 1-.708.708L14 7.707l-1.146 1.147a.5.5 0 0 1-.708-.708L13.293 7l-1.147-1.146a.5.5 0 0 1 0-.708z"/>
+                                        <div class="col-1 hide join-lobby" id="${i}-${lobbiesInfo[i].idhash}-joinlobby">
+                                            <svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-arrow-right-circle-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                                <path fill-rule="evenodd" d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-11.5.5a.5.5 0 0 1 0-1h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5z"/>
                                             </svg>
                                         </div>
                                     </div>
@@ -44,7 +44,44 @@ $(document).ready(() => {
             $("#alert .msg").text(err.responseJSON);
             $("#alert").fadeIn(500);
             console.log(err);
-        }
+        };
 
-    
+        let selectLast;
+
+    $(".lobby-select").on("click", function() {
+        event.preventDefault();
+        // let selectIdHash = this.children[0].children[0].children[0].children[0].id;
+        // let selectTargIdHash = event.target.id;
+        let select = parseInt(event.target.id.split("-")[0])
+        let idHash = event.target.id.split("-")[1]
+        // && select !== selectLast
+        if(idHash === lobbiesInfo[select].idhash && select !== selectLast) {
+            $(".join-lobby").addClass("hide")
+            $(".add-lobby").removeClass("hide")
+            
+            $(`#${select}-${idHash}-addlobby`).addClass("hide")
+            $(`#${select}-${idHash}-joinlobby`).removeClass("hide")
+            selectLast = select;
+        }else if(select === selectLast) {
+            joinlobby(idHash);
+        }else{
+            selectLast = "";
+            $(`#${select}-${idHash}-addlobby`).removeClass("hide")
+            $(`#${select}-${idHash}-joinlobby`).addClass("hide")
+        };
+    });
+
+    function joinlobby(idHash){
+        console.log(idHash)
+        $.post(`/api/lobby/join/${idHash}`)
+        .then(res => console.log(res))
+
+        .catch(handleLoginErr);
+
+        function handleLoginErr(err) {
+            $("#alert .msg").text(err.responseJSON);
+            $("#alert").fadeIn(500);
+            console.log(err);
+        }
+    };
 });
